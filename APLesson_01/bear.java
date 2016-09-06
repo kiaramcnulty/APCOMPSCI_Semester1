8835b9eb405648948ba6860425e40a7cc89a10e7
kiaramcnulty <kiaramcnulty@gmail.com>
2016-09-06 11:03:31 -0700

    Added Files

---
+++ APLesson_01/bear.java
@@ -0,0 +1,15 @@
+public class bear
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" __         __");
+		System.out.println("/  \\.-\"\"\"-./  \\");
+		System.out.println("\\    -   -    /");
+		System.out.println(" |   o   o   |");
+		System.out.println(" \\  .-'''-.  /");
+		System.out.println("  '-\\__Y__/-'");
+		System.out.println("     `---`");
+		System.out.println("This is a bear");
+		
+	}
+} 
---
+++ APLesson_01/monkey.java
@@ -0,0 +1,11 @@
+public class monkey
+{
+	public static void main(String[]args)
+	{
+		System.out.println("   /~\\");
+		System.out.println(" C oo");
+		System.out.println(" _( ^)");
+		System.out.println("/   ~\\");
+		System.out.println("It's a monkey!");
+	}
+} 
