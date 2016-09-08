606636aed64b65d63198ac955e1d03286a697245
kiaramcnulty <kiaramcnulty@gmail.com>
2016-09-08 11:26:54 -0700

    Added Lesson 3

---
+++ APLesson_03/Lesson_03.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; //import statement
+
+public class Lesson_03
+{
+	public static void main(String[]args)
+	{
+		//instantiate the new Scanner object "keyboard"
+		Scanner keyboard = new Scanner(System.in);
+		
+		//prompt for user input
+		System.out.println("Who is your favorite teacher?");
+		
+		String teacher = keyboard.next();
+		
+		//print the results
+		System.out.println("Okay! " + teacher + " is very good.");
+		
+		
+		
+	}
+}
